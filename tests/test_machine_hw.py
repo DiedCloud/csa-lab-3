df75f0b55fe61d03c7c9cf20af2ee2109341efa8
DiedCloud <fkirill2604@gmail.com>
2024-06-19 23:00:12 +0300

    feat: pyproject, poerty, tests ...

---
+++ tests/test_machine_hw.py
@@ -0,0 +1,41 @@
+import contextlib
+import io
+import logging
+import os
+import tempfile
+
+import machine_hw
+import pytest
+import translator
+
+
+@pytest.mark.golden_test("golden/*.yml")
+def test_bar(golden, caplog):
+    caplog.set_level(logging.DEBUG)
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        source = os.path.join(tmpdir, "source.fth")
+        input_stream = os.path.join(tmpdir, "input.txt")  # [ord(i) for i in golden["stdin"].rstrip("\n")]
+        target = os.path.join(tmpdir, "source.json")
+
+        # Записываем входные данные в файлы. Данные берутся из теста.
+        with open(source, "w", encoding="utf-8") as file:
+            file.write(golden["in_source"])
+        with open(input_stream, "w", encoding="utf-8") as file:
+            file.write(golden["in_stdin"])
+
+        # Запускаем транслятор и собираем весь стандартный вывод в переменную
+        # stdout
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            translator.main(source, target)
+            print("=" * 60)
+            machine_hw.main(target, input_stream)
+
+        # Выходные данные также считываем в переменные.
+        with open(target, encoding="utf-8") as file:
+            code = file.read()
+
+        # Проверяем, что ожидания соответствуют реальности.
+        assert code == golden.out["out_code"]
+        assert stdout.getvalue() == golden.out["out_stdout"]
+        assert caplog.text == golden.out["out_log"]
